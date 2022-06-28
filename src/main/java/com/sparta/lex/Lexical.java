d33d48cfea24964508679a8425a53cd7412c670a
Dhrushil Sedani <dhrushilsedani@Dhrushils-MacBook-Pro.local>
2022-06-28 11:37:58 +0100

    Initial commit

---
+++ src/main/java/com/sparta/lex/Lexical.java
@@ -0,0 +1,11 @@
+package com.sparta.lex;
+
+import java.util.Arrays;
+
+public class Lexical {
+
+    public void numOfChar(String input) {
+        String[] splitInput = input.split(" ");
+        System.out.println(Arrays.toString(splitInput));
+    }
+}
