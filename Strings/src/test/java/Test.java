60f1047f574ea0aadf651b7c6efa1c46cd9682bc
Jamie Scofield <jamies_1048@outlook.com>
2022-06-28 12:28:19 +0100

    added testing for num of char and words

--- Strings/src/test/java/Test.java
+++ Strings/src/test/java/Test.java
@@ -15,10 +15,23 @@ public class Testing {
         Assertions.assertEquals(5,lexical.numOfChar("hello"));
     }
     @Test
+    @DisplayName("Large number of characters")
+    void largeChar(){
+        Lexical lexical = new Lexical();
+        Assertions.assertEquals(300,lexical.numOfChar("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."));
+    }
+    @Test
+    @DisplayName("no character check")
+    void zeroChar(){
+        Lexical lexical = new Lexical();
+        Assertions.assertEquals(0,lexical.numOfChar(""));
+    }
+    @Test
     @DisplayName("Number of words")
     void testNumberOfWords(){
         Lexical lexical = new Lexical();
         Assertions.assertEquals(3,lexical.numOfWords("how are you"));
     }
+
 
 }
